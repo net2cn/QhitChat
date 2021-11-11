8251acc739feeecf2f62e844a8d578c6eae21940
net2cn <mcopener@gmail.com>
2021-11-11 18:39:30 +0800

    Add basic layout for MainWindow
    Chat box is not added yet.
    
    Signed-off-by: net2cn <mcopener@gmail.com>

---
+++ QhitChat-Client/QhitChat-Client/Windows/Controls/Native.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace QhitChat_Client.Windows
+{
+    public static class Native
+    {
+
+        [DllImport("user32")]
+        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
+
+        [DllImport("user32")]
+        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        public static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam, int minWidth, int minHeight)
+        {
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            // Adjust the maximized size and position to fit the work area of the correct monitor
+            int MONITOR_DEFAULTTONEAREST = 0x00000002;
+            IntPtr monitor = Native.MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero)
+            {
+
+                Native.MONITORINFO monitorInfo = new Native.MONITORINFO();
+                Native.GetMonitorInfo(monitor, monitorInfo);
+                Native.RECT rcWorkArea = monitorInfo.rcWork;
+                Native.RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left)-10;    // Add 20 offset to fill the whole area. Otherwise there will be some extra space. (Maybe use multiply?)
+                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top)-10;
+                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left)+20;
+                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top)+20;
+                mmi.ptMinTrackSize.x = minWidth;
+                mmi.ptMinTrackSize.y = minHeight;
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
+        }
+
+
+        /// <summary>
+        /// POINT aka POINTAPI
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            /// <summary>
+            /// x coordinate of point.
+            /// </summary>
+            public int x;
+            /// <summary>
+            /// y coordinate of point.
+            /// </summary>
+            public int y;
+
+            /// <summary>
+            /// Construct a point of coordinates (x,y).
+            /// </summary>
+            public POINT(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public class MONITORINFO
+        {       
+            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+            public RECT rcMonitor = new RECT();       
+            public RECT rcWork = new RECT();        
+            public int dwFlags = 0;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+
+            public static readonly RECT Empty = new RECT();
+
+            public int Width
+            {
+                get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
+            }
+            public int Height
+            {
+                get { return bottom - top; }
+            }
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                this.left = left;
+                this.top = top;
+                this.right = right;
+                this.bottom = bottom;
+            }
+
+        }
+    }
+}
--- QhitChat-Client/QhitChat-Client/Windows/MainWindow.xaml.cs
+++ QhitChat-Client/QhitChat-Client/Windows/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -20,7 +21,19 @@ namespace QhitChat_Client.Windows
     {
         public MainWindow()
         {
+            SourceInitialized += Window_SourceInitialized;
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TitleLabel.Content = Core.Configuration.TITLE;
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(handle)?.AddHook(WindowProc);
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -36,17 +49,31 @@ namespace QhitChat_Client.Windows
                 window.WindowState = WindowState.Minimized;
         }
 
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
         {
-            base.OnMouseLeftButtonDown(e);
+            Window window = GetWindow(this);
 
-            //让窗口动起来
-            DragMove();
+            if (window.WindowState != WindowState.Maximized)
+            {
+                window.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = WindowState.Normal;
+            }
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            TitleLabel.Content = Core.Configuration.TITLE;
+            switch (msg)
+            {
+                case 0x0024:
+                    Native.WmGetMinMaxInfo(hwnd, lParam, (int)MinWidth, (int)MinHeight);
+                    handled = true;
+                    break;
+            }
+
+            return (IntPtr)0;
         }
     }
 }
